ae5c2c62244fb2e923bf32b254a65f69eb6efbce
황상한 <55230278+nmc2711@users.noreply.github.com>
2025-07-08 15:59:43 +0900

    feat: next auth . google login 연동

--- components/auth-button.tsx
+++ components/auth-button.tsx
@@ -1,6 +1,53 @@
+'use client';
+
 import React from 'react';
 import { Button } from './ui/button';
+import { Avatar, AvatarFallback, AvatarImage } from './ui/avatar';
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from '@/components/ui/dropdown-menu';
+import { useSession, signIn, signOut } from 'next-auth/react';
 
 export default function AuthButton() {
-  return <Button variant='outline'>회원가입</Button>;
+  const { data: session, status } = useSession();
+
+  if (status === 'loading') return <Button variant='ghost'>로딩중..</Button>;
+  if (!session) {
+    return (
+      <Button variant='outline' onClick={() => signIn('google')}>
+        회원가입
+      </Button>
+    );
+  }
+  return (
+    <DropdownMenu>
+      <DropdownMenuTrigger asChild>
+        <Button variant='ghost' className='relative h-8 w-8 rounded-full'>
+          <Avatar className='h-8 w-8'>
+            <AvatarImage
+              src={session.user?.image || ''}
+              alt={session.user?.name || ''}
+            />
+            <AvatarFallback>{session.user?.name?.[0]}</AvatarFallback>
+          </Avatar>
+        </Button>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent className='w-56' align='end' forceMount>
+        <DropdownMenuItem className='font-normal'>
+          <div className='flex flex-col space-y-1'>
+            <p className='text-sm font-medium leading-none'>
+              {session.user?.name}
+            </p>
+            <p className='text-xs leading-none text-muted-foreground'>
+              {session.user?.email}
+            </p>
+          </div>
+        </DropdownMenuItem>
+        <DropdownMenuItem onClick={() => signOut()}>Sign out</DropdownMenuItem>
+      </DropdownMenuContent>
+    </DropdownMenu>
+  );
 }
